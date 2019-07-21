9bd0d835599bdb9106e9601e9356bcc2b6b40486
Justin McCandless <jmccandless@google.com>
2019-07-21 16:58:34 -0700

    Complete fib with tests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fib(number) {
-  if (number < 2) {
-    return number;
+module.exports = function fib(number) {
+  if (typeof number !== 'number' || number < 0) {
+    throw new Error('Invalid input');
+  }
+  if (typeof number !== 'number' || number < 2) {
+    return 1;
   }
   return fib(number - 1) + fib(number - 2);
-}
+};
--- tests.spec.js
+++ tests.spec.js
@@ -1,9 +1,21 @@
-var assert = require('assert');
+const assert = require('assert');
+const fib = require('./index.js');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('fib', function() {
+  it('returns the base case fibonacci numbers', function() {
+    assert.equal(fib(0), 1);
+    assert.equal(fib(1), 1);
+  });
+
+  it('calculates the first few fibonacci numbers', function() {
+    assert.equal(fib(2), 2);
+    assert.equal(fib(3), 3);
+    assert.equal(fib(4), 5);
+    assert.equal(fib(5), 8);
+  });
+
+  it('throws an error for invalid input', function() {
+    assert.throws(fib.bind(null, -1), Error);
+    assert.throws(fib.bind(null, 'a'), Error);
   });
 });
